e3967a329164f8725a13b19ecc5e68cc9d81405c
Bijanlee <bijan@codelikely.com>
2020-06-01 11:44:57 -0700

    End of Section 10

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API.Helpers
                     opt.MapFrom(src => src.DateOfBirth.CalculateAge()));    
             
             CreateMap<Photo, PhotosForDetailsDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
